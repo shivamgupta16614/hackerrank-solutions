48b7f87ef4fd44f9ad5f9c83557d138e1878fd74
shivamgupta16614 <53917065+shivamgupta16614@users.noreply.github.com>
2019-08-28 23:10:57 +0530

    Add files via upload

---
+++ dynamic programming/fibonacci modified.c
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using boost::multiprecision::cpp_int;
+
+cpp_int fib(cpp_int a, cpp_int b, unsigned int n)
+{
+    for(unsigned int i = 2; i < n; ++i)
+    {
+        cpp_int temp = a + b*b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+int main()
+{
+    unsigned int a, b, n;
+
+    std::cin >> a >> b >> n;
+    std::cout << fib(a, b, n);
+}
